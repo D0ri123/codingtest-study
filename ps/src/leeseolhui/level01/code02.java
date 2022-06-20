210bb2bf341982ee38695080584950868658bae8
d0ri123 <lsh81550@gmail.com>
2022-06-20 11:54:43 +0900

    add problem 1001

---
+++ ps/src/leeseolhui/level01/code02.java
@@ -0,0 +1,15 @@
+package leeseolhui.level01;
+
+import java.util.Scanner;
+
+public class code02 {
+    public static void main(String[] args){
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a - b);
+    }
+}
