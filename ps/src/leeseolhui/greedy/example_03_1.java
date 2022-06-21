b9edd833e92806fb27f4ed548b415638349291cc
d0ri123 <lsh81550@gmail.com>
2022-06-21 14:33:44 +0900

    solve the greedy algorithm 3-1

---
+++ ps/src/leeseolhui/greedy/example_03_1.java
@@ -0,0 +1,21 @@
+package leeseolhui.greedy;
+
+public class example_03_1 {
+    public static void main(String[] args){
+        int change = 1260;
+
+        int a = change / 500; //a는 500원
+        change = change - (500*a);
+
+        int b = change / 100; //b는 100원
+        change = change - (100*b);
+
+        int c = change / 50; //c는 50원
+        change = change - (50*c);
+
+        int d = change / 10; //d는 10원
+        change = change - (10*d);
+
+        System.out.println(a+b+c+d);
+    }
+}
