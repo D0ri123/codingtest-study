87b98ad6289e918268e17b3fab7fc93cdd21465e
d0ri123 <lsh81550@gmail.com>
2022-07-04 14:06:45 +0900

    delete file

--- ps/src/leeseolhui/baekjoon/level01/Test.java
+++
@@ -1,8 +0,0 @@
-package leeseolhui.baekjoon.level01;
-
-public class Test {
-    public static void main(String[] args){
-        System.out.println("hello, java!");
-        System.out.println("java babo");
-    }
-}
--- ps/src/leeseolhui/baekjoon/level01/Test1.java
+++
@@ -1,7 +0,0 @@
-package leeseolhui.baekjoon.level01;
-
-public record Test1() {
-    public static void main(String[] args){
-        System.out.println("hello, world!");
-    }
-}
