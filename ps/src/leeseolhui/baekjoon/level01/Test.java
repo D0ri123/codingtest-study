15710e3e64375f988118ee5a131aad5d061128b3
d0ri123 <lsh81550@gmail.com>
2022-07-04 09:32:46 +0900

    modified code

--- ps/src/leeseolhui/baekjoon/level01/Test.java
+++ ps/src/leeseolhui/baekjoon/level01/Test.java
@@ -3,5 +3,6 @@ package leeseolhui.baekjoon.level01;
 public class Test {
     public static void main(String[] args){
         System.out.println("hello, java!");
+        System.out.println("java babo");
     }
 }
