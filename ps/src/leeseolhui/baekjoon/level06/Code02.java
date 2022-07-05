efe2cc588e5b4ac502f6a1812f6e85fa22808bef
d0ri123 <lsh81550@gmail.com>
2022-07-05 12:48:14 +0900

    solve baekjoon 11720

--- ps/src/leeseolhui/baekjoon/level06/Code02.java
+++ ps/src/leeseolhui/baekjoon/level06/Code02.java
@@ -1,2 +1,21 @@
-package leeseolhui.baekjoon.level06;public class Code02 {
+package leeseolhui.baekjoon.level06;
+
+import java.util.Scanner;
+
+public class Code02 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int sum = 0;
+
+        int n = sc.nextInt();
+        String num = sc.next();
+        String[] arr = num.split("");
+
+        for(int i=0; i<arr.length; i++){
+            int a = Integer.parseInt(arr[i]);
+            sum += a;
+        }
+
+        System.out.println(sum);
+    }
 }
