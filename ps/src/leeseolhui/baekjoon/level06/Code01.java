68bc1476dd0d1a7bd11ec0a5d851c6bcf55559a3
d0ri123 <lsh81550@gmail.com>
2022-07-05 12:47:37 +0900

    solve baekjoon 11654

--- ps/src/leeseolhui/baekjoon/level06/Code01.java
+++ ps/src/leeseolhui/baekjoon/level06/Code01.java
@@ -1,2 +1,14 @@
-package leeseolhui.baekjoon.level06;public class Code01 {
+package leeseolhui.baekjoon.level06;
+
+import java.util.Scanner;
+
+public class Code01 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.nextLine().charAt(0);
+        int res = (int)a;
+
+        System.out.println(res);
+    }
 }
