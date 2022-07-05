4fc8a31227e1bd744f9d4631455738238db4b4d2
d0ri123 <lsh81550@gmail.com>
2022-07-05 12:46:50 +0900

    solve baekjoon 25083

--- ps/src/leeseolhui/baekjoon/level01/Code012.java
+++ ps/src/leeseolhui/baekjoon/level01/Code012.java
@@ -1,2 +1,12 @@
-package leeseolhui.baekjoon.level01;public class Code012 {
+package leeseolhui.baekjoon.level01;
+
+public class Code012 {
+    public static void main(String[] args){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
 }
