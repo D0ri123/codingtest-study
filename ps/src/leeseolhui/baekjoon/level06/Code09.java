4a49918ee09be6f0314643a5837e985108c5ee67
d0ri123 <lsh81550@gmail.com>
2022-07-07 19:27:29 +0900

    solve baekjoon 2941

--- ps/src/leeseolhui/baekjoon/level06/Code09.java
+++ ps/src/leeseolhui/baekjoon/level06/Code09.java
@@ -1,2 +1,18 @@
-package leeseolhui.baekjoon.level06;public class Code09 {
+package leeseolhui.baekjoon.level06;
+
+import java.util.Scanner;
+
+public class Code09 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String cro[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        String input = sc.nextLine();
+
+        for(int i=0; i<cro.length; i++){
+            if(input.contains(cro[i]))
+                input = input.replace(cro[i], "!");
+        }
+        System.out.println(input.length());
+    }
 }
