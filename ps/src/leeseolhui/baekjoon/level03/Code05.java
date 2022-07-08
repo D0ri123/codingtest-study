3af7c9f9fa8bd287126f3ba8f9ffed9b5175e103
d0ri123 <lsh81550@gmail.com>
2022-07-08 14:13:00 +0900

    solve baekjoon 2741

---
+++ ps/src/leeseolhui/baekjoon/level03/Code05.java
@@ -0,0 +1,14 @@
+package leeseolhui.baekjoon.level03;
+
+import java.util.Scanner;
+
+public class Code05 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
