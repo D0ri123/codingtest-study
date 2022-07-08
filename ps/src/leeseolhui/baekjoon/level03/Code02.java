e6f60eb24f74a46fcc2fb8999f231442f7d7246b
d0ri123 <lsh81550@gmail.com>
2022-07-08 14:04:42 +0900

    solve baekjoon 10950

---
+++ ps/src/leeseolhui/baekjoon/level03/Code02.java
@@ -0,0 +1,16 @@
+package leeseolhui.baekjoon.level03;
+
+import java.util.Scanner;
+
+public class Code02 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+
+        for(int i=0; i<test; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
