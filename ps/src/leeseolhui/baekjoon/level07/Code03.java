d9d23d4cc50631f4bce68bed113bda9c4c0bc8ad
d0ri123 <lsh81550@gmail.com>
2022-07-12 14:02:36 +0900

    solve baekjoon 1193

--- ps/src/leeseolhui/baekjoon/level07/Code03.java
+++ ps/src/leeseolhui/baekjoon/level07/Code03.java
@@ -1,2 +1,28 @@
-package leeseolhui.baekjoon.level07;public class Code03 {
+package leeseolhui.baekjoon.level07;
+
+import java.util.Scanner;
+
+public class Code03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int prev = 0; // 직전 대각선 누적합
+        int cnt = 1; // 해당 대각선 개수
+
+        while(true){
+            if(a <= prev + cnt){
+                if(cnt % 2 == 1){ //해당 대각선 개수가 홀수일 경우
+                    System.out.print((cnt - (a - prev - 1)) + "/" + (a - prev));
+                    break;
+                }
+                else{ //해당 대각선 개수가 짝수일 경우
+                    System.out.print((a-prev) + "/" + (cnt - (a - prev - 1)));
+                    break;
+                }
+            }else{
+                prev += cnt;
+                cnt++;
+            }
+        }
+    }
 }
