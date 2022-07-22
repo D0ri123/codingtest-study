8d591f5d04f9c483e1fd19faefddcc851f8733db
d0ri123 <lsh81550@gmail.com>
2022-07-22 13:12:46 +0900

    solve beakjoon 1929

--- ps/src/leeseolhui/baekjoon/level08/Code04.java
+++ ps/src/leeseolhui/baekjoon/level08/Code04.java
@@ -1,2 +1,29 @@
-package leeseolhui.baekjoon.level08;public class Code04 {
+package leeseolhui.baekjoon.level08;
+
+import java.util.Scanner;
+
+public class Code04 {
+
+    public static boolean isPrime(int n){
+            if(n<2) return false;
+            for(int j=2; j<=Math.sqrt(n); j++){
+                if(n%j==0)
+                    return false;
+            }
+            return true;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        boolean b;
+
+        for(int i=m; i<=n; i++){
+           b = isPrime(i);
+           if(b==true){
+               System.out.println(i);
+           }
+        }
+    }
 }
