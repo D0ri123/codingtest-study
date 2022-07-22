a117cc2dc0c0d165c94d8413c460cc8815725128
d0ri123 <lsh81550@gmail.com>
2022-07-22 13:14:18 +0900

    solve baekjoon 10872

--- ps/src/leeseolhui/baekjoon/level09/Code01.java
+++ ps/src/leeseolhui/baekjoon/level09/Code01.java
@@ -1,2 +1,18 @@
-package leeseolhui.baekjoon.level09;public class Code01 {
+package leeseolhui.baekjoon.level09;
+
+import java.util.Scanner;
+
+public class Code01 {
+
+    public static int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
 }
