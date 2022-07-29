c8ec4ef661f4dad35df6d110568b8f27fc509867
d0ri123 <lsh81550@gmail.com>
2022-07-29 15:19:27 +0900

    solve baekjoon 10989

--- ps/src/leeseolhui/baekjoon/level11/Code03.java
+++ ps/src/leeseolhui/baekjoon/level11/Code03.java
@@ -1,2 +1,30 @@
-package leeseolhui.baekjoon.level11;public class Code03 {
+package leeseolhui.baekjoon.level11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Code03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] result = new int[10001];
+
+        int n = Integer.parseInt(bf.readLine());
+
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(bf.readLine());
+            result[num]++;
+        }
+
+        for(int i=0; i<result.length; i++){
+            if(result[i] != 0){
+                for(int j=1; j<=result[i]; j++){
+                    sb.append(i).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 }
