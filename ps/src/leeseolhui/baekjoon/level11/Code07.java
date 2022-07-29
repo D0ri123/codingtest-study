e19047cc8c4e4ca85953bb95a4b462472374e8c6
d0ri123 <lsh81550@gmail.com>
2022-07-29 15:22:01 +0900

    solve baekjoon 11651

--- ps/src/leeseolhui/baekjoon/level11/Code07.java
+++ ps/src/leeseolhui/baekjoon/level11/Code07.java
@@ -1,2 +1,36 @@
-package leeseolhui.baekjoon.level11;public class Code07 {
+package leeseolhui.baekjoon.level11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Code07 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String s = bf.readLine();
+            String[] str = s.split(" ");
+            arr[i][0] = Integer.parseInt(str[0]);
+            arr[i][1] = Integer.parseInt(str[1]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[1]==o2[1]){
+                return Integer.compare(o1[0],o2[0]);
+            }
+            else{
+                return Integer.compare(o1[1],o2[1]);
+            }
+        });
+
+
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
 }
