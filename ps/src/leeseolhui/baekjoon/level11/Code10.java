8fe983d56e5a9523f571d2b3a7900c06650e22eb
d0ri123 <lsh81550@gmail.com>
2022-07-29 15:23:46 +0900

    solve baekjoon 18870

--- ps/src/leeseolhui/baekjoon/level11/Code10.java
+++ ps/src/leeseolhui/baekjoon/level11/Code10.java
@@ -1,2 +1,36 @@
-package leeseolhui.baekjoon.level11;public class Code10 {
+package leeseolhui.baekjoon.level11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Code10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
+        for(int a:sorted){
+            if(!map.containsKey(a))
+                map.put(a, idx++);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i:arr){
+            sb.append(map.get(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
