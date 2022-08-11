dd2b196545283fc97bdc6ea93f0929b9a79eb43b
d0ri123 <lsh81550@gmail.com>
2022-08-12 07:41:26 +0900

    solve baekjoon 7287

---
+++ ps/src/leeseolhui/implementation/Code05.java
@@ -0,0 +1,9 @@
+package leeseolhui.implementation;
+
+public class Code05 {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("108").append("\n").append("lsh8155");
+        System.out.println(sb);
+    }
+}
