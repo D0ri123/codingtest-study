275c663fcf2d2a6b105a410a23f6413f494f73f6
d0ri123 <lsh81550@gmail.com>
2022-08-16 14:41:48 +0900

    solve baekjoon 1012

--- ps/src/leeseolhui/dfs_bfs/Code05.java
+++ ps/src/leeseolhui/dfs_bfs/Code05.java
@@ -1,2 +1,51 @@
-package leeseolhui.dfs_bfs;public class Code05 {
+package leeseolhui.dfs_bfs;
+
+import java.util.Scanner;
+
+public class Code05 {
+    public static int m,n,k;
+    static int[][] graph;
+
+    public static boolean dfs(int x, int y){ //주변에 1이 있는지 탐색
+        if(x<=-1 || x>=n || y<=-1 || y>=m) {
+            return false;
+        }
+        if(graph[x][y]==1){
+            graph[x][y] = 0;
+            dfs(x-1,y);
+            dfs(x+1,y);
+            dfs(x,y-1);
+            dfs(x,y+1);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException{
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int test = sc.nextInt();
+        for(int i=0; i<test; i++){
+            int result = 0;
+            n = sc.nextInt();
+            m = sc.nextInt();
+            k = sc.nextInt();
+            sc.nextLine();
+
+            graph = new int[n][m];
+            for(int j=1; j<=k; j++){
+                graph[sc.nextInt()][sc.nextInt()] = 1;
+            }
+
+            for(int a=0; a<n; a++){
+                for(int b=0; b<m; b++){
+                    if(dfs(a, b))
+                        result++;
+                }
+            }
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
