5c299060e020e51dfbfbb56a9c3d62d1e26e18bc
d0ri123 <lsh81550@gmail.com>
2022-08-29 13:36:20 +0900

    solve baekjoon 2805

--- ps/src/leeseolhui/binary_search/Code01.java
+++ ps/src/leeseolhui/binary_search/Code01.java
@@ -1,2 +1,52 @@
-package leeseolhui.binary_search;public class Code01 {
+package leeseolhui.binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Code01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] tree = new int[n];
+
+        int start = 0;
+        int end = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<tree.length; i++){
+            tree[i] = Integer.parseInt(st.nextToken());
+            end = Math.max(end, tree[i]);
+        }
+
+        Arrays.sort(tree);
+
+        int result = 0;
+        while(start<=end){
+            long total = 0;
+            int mid = (start+end)/2;
+
+            for(int i=0; i<n; i++){
+                if(tree[i]>mid){
+                    total += tree[i]-mid;
+                }
+            }
+
+            if(total<m){
+                end = mid - 1;
+            } else{
+                start = mid + 1;
+                result = mid;
+            }
+        }
+        System.out.println(result);
+    }
 }
+
+
