9024f157d0398cf23aca7ee56a235c9efcf2fb83
d0ri123 <lsh81550@gmail.com>
2022-09-28 18:27:38 +0900

    solve baekjoon 7795

--- ps/src/leeseolhui/binary_search/Code18.java
+++ ps/src/leeseolhui/binary_search/Code18.java
@@ -1,2 +1,33 @@
-package leeseolhui.binary_search;public class Code18 {
+package leeseolhui.binary_search;
+
+/**
+ * baekjoon 2467 - 용액
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Code18 {
+    public static int solution(int[] arr, int n){
+    return 1;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+    }
 }
