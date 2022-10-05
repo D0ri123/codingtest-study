3b076938c5ecf3dfc91fd89a5976e5bfa35aa4b0
d0ri123 <lsh81550@gmail.com>
2022-10-05 19:36:59 +0900

    학급회장 refactoring

--- ps/src/leeseolhui/hash_map/Code01.java
+++ ps/src/leeseolhui/hash_map/Code01.java
@@ -4,37 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 학급 회장
  */
 
 public class Code01 {
-    public static String solution(String[] arr, int n){
-        int a=1,b=1,c=1,d=1,e=1;
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            if(arr[i].equals("A")) map.put("A", a++);
-            else if(arr[i].equals("B")) map.put("B", b++);
-            else if(arr[i].equals("C")) map.put("C", c++);
-            else if(arr[i].equals("D")) map.put("D", d++);
-            else map.put("E", e++);
+    public static char solution(int n, String s){
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char x: s.toCharArray()){
+            map.put(x, map.getOrDefault(x,0)+1);
         }
 
-        String[] strings = {"A", "B", "C", "D", "E"};
         int max = Integer.MIN_VALUE;
-        for(int i=0; i<5; i++){
-            max = Math.max(max, map.get(strings[i]));
+        for(char key : map.keySet()){
+            if(map.get(key)>max){
+                max = map.get(key);
+                answer = key;
+            }
         }
-
-        String ans = "";
-        for(String str:map.keySet()) {
-            if(max==map.get(str))
-                ans = str;
-        }
-
-        return ans;
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
@@ -43,9 +33,8 @@ public class Code01 {
 
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        String[] arr = str.split("");
 
-        sb.append(solution(arr, n));
+        sb.append(solution(n, str));
         System.out.println(sb);
     }
 }
