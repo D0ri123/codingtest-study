38e6fe2c1efcba385db8f8a39076683c777fd87c
d0ri123 <lsh81550@gmail.com>
2022-11-16 19:54:58 +0900

    solve Problem1,String

---
+++ ps/src/leeseolhui/string/Code01.java
@@ -0,0 +1,41 @@
+package leeseolhui.string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Code01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        solution(str);
+    }
+
+    public static void solution(String answer){
+        StringBuilder sb = new StringBuilder();
+
+        char[] chars = answer.toCharArray();
+        boolean[] isChar = new boolean[chars.length];
+        List<Character> list = new ArrayList<>();
+
+        Arrays.fill(isChar, false);
+
+        for(int i=0; i<chars.length; i++){
+            char alphabet = chars[i];
+            if((alphabet>=65 && alphabet<=90) || (alphabet>=97 && alphabet<=122)){
+                list.add(alphabet);
+                isChar[i] = true;
+            }
+        }
+
+        int lastIdx = list.size()-1;
+        for(int i=0; i<chars.length; i++){
+            if(isChar[i])
+                sb.append(list.get(lastIdx--));
+            else
+                sb.append(chars[i]);
+        }
+        System.out.println(sb);
+    }
+}
