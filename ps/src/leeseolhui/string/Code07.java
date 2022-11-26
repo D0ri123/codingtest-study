a89af8e94d5282447c849e5b80b1b937099bb849
d0ri123 <lsh81550@gmail.com>
2022-11-26 16:22:44 +0900

    solve: 문자열 압축

--- ps/src/leeseolhui/string/Code07.java
+++ ps/src/leeseolhui/string/Code07.java
@@ -1,2 +1,34 @@
-package leeseolhui.string;public class Code07 {
+package leeseolhui.string;
+
+// 문자열 압축
+
+import java.util.Scanner;
+
+public class Code07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(solution(word));
+    }
+
+    public static String solution(String word){
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+        int i=1;
+        while(word.length()>0){
+            char alphabet = word.charAt(0);
+            if(alphabet == word.charAt(i)) {
+                i++;
+                count++;
+            }
+            else{
+                sb.append(alphabet).append(count);
+                word = word.substring(i);
+                count = 1;
+            }
+        }
+
+        return sb.toString();
+    }
 }
