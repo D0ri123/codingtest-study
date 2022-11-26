90cf28c4e157863520cfbdedd0a985d75b197bfd
d0ri123 <lsh81550@gmail.com>
2022-11-26 15:29:27 +0900

    refactor: 특정 문자 뒤집기

--- ps/src/leeseolhui/string/Code01.java
+++ ps/src/leeseolhui/string/Code01.java
@@ -2,42 +2,33 @@ package leeseolhui.string;
 
 //특정 문자 뒤집기
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Code01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        solution(str);
+        System.out.println(solution(str));
     }
 
-    public static void solution(String answer){
-        StringBuilder sb = new StringBuilder();
+    public static String solution(String answer){
+        int lt = 0;
+        int rt = answer.length()-1;
 
         char[] chars = answer.toCharArray();
-        boolean[] isChar = new boolean[chars.length];
-        List<Character> list = new ArrayList<>();
 
-        Arrays.fill(isChar, false);
-
-        for(int i=0; i<chars.length; i++){
-            char alphabet = chars[i];
-            if((alphabet>=65 && alphabet<=90) || (alphabet>=97 && alphabet<=122)){
-                list.add(alphabet);
-                isChar[i] = true;
+        while(lt<rt){
+            if(!Character.isAlphabetic(chars[lt])) lt++;
+            else if(!Character.isAlphabetic(chars[rt])) rt--;
+            else{
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+                lt++;
+                rt--;
             }
         }
 
-        int lastIdx = list.size()-1;
-        for(int i=0; i<chars.length; i++){
-            if(isChar[i])
-                sb.append(list.get(lastIdx--));
-            else
-                sb.append(chars[i]);
-        }
-        System.out.println(sb);
+        return String.valueOf(chars);
     }
 }
---
+++ ps/src/leeseolhui/string/Code07.java
@@ -0,0 +1,2 @@
+package leeseolhui.string;public class Code07 {
+}
