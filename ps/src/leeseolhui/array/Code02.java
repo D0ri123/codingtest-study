7ca7c4625df26057a9940cfafe4a74c393ca72d6
d0ri123 <lsh81550@gmail.com>
2022-11-30 18:47:52 +0900

    solve 보이는 학생

---
+++ ps/src/leeseolhui/array/Code02.java
@@ -0,0 +1,30 @@
+package leeseolhui.array;
+
+//보이는 학생
+
+import java.util.Scanner;
+
+public class Code02 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] students = new int[n];
+
+        for(int i=0; i<n; i++){
+            students[i] = sc.nextInt();
+        }
+        System.out.println(solution(students));
+    }
+
+    public static int solution(int[] arr){
+        int count = 1;
+        int max = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]>max) {
+                count++;
+                max = Math.max(max, arr[i]);
+            }
+        }
+        return count;
+    }
+}
