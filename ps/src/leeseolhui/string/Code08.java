2319d625c1a96b770a6f8f819c387a4818f1c669
d0ri123 <lsh81550@gmail.com>
2022-11-30 16:56:31 +0900

    solve 암호

--- ps/src/leeseolhui/string/Code08.java
+++ ps/src/leeseolhui/string/Code08.java
@@ -1,7 +1,26 @@
 package leeseolhui.string;
 
+//암호
+
+import java.util.Scanner;
+
 public class Code08 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String password = sc.next();
 
+        System.out.println(change(n,password));
+    }
+
+    public static String change(int n, String password){
+        String answer = "";
+        for(int i=0; i<n; i++){
+            String tmp = password.substring(0,7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char)num;
+            password = password.substring(7);
+        }
+        return answer;
     }
 }
