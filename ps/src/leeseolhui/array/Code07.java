6294f3376f22ba4a34e94c89d9e5934f2c8fa5db
d0ri123 <lsh81550@gmail.com>
2022-12-01 13:28:46 +0900

    refactor 점수계산

--- ps/src/leeseolhui/array/Code07.java
+++ ps/src/leeseolhui/array/Code07.java
@@ -8,31 +8,23 @@ public class Code07 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
 
         int[] scores = new int[n];
         for(int i=0; i<n; i++){
             scores[i] = sc.nextInt();
         }
-
         solution(scores);
     }
 
     public static void solution(int[] scores){
-        int[] scoreTable = new int[scores.length];
-        int count = 1;
+        int count = 0;
         int sum = 0;
         for(int i=0; i<scores.length; i++){
-            if(scores[i]==1)
-                scoreTable[i] = count++;
-            if(scores[i]==0) {
-                scoreTable[i] = 0;
-                count = 1;
-            }
-        }
-
-        for(int eachScore : scoreTable){
-            sum += eachScore;
+            if(scores[i]==1) {
+                count++;
+                sum += count;
+            } else if(scores[i]==0)
+                count=0;
         }
         System.out.println(sum);
     }
