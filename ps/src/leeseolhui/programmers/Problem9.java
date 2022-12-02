c3bfa71ef1c90c52d82902fa8e4616feee9afdcf
d0ri123 <lsh81550@gmail.com>
2022-12-02 15:10:06 +0900

    solve 2*N타일링

--- ps/src/leeseolhui/programmers/Problem9.java
+++ ps/src/leeseolhui/programmers/Problem9.java
@@ -1,2 +1,24 @@
-package leeseolhui.programmers;public class Problem9 {
+package leeseolhui.programmers;
+
+// 2*n 타일링
+
+import java.util.Scanner;
+
+public class Problem9 {
+    public static int[] dy;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n+1];
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n){
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i=3; i<=n; i++){
+            dy[i] = (dy[i-1] + dy[i-2]) % 1000000007;
+        }
+        return dy[n];
+    }
 }
