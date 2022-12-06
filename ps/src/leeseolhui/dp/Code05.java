843596f1ec4024ae4fda794a06e8840f3183f87a
d0ri123 <lsh81550@gmail.com>
2022-12-06 12:01:52 +0900

    solve 동전교환-냅색알고

--- ps/src/leeseolhui/dp/Code05.java
+++ ps/src/leeseolhui/dp/Code05.java
@@ -1,2 +1,38 @@
-package leeseolhui.dp;public class Code05 {
+package leeseolhui.dp;
+
+//동전교환(냅색 알고리즘)
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Code05 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] coin = new int[n];
+        for(int i=0; i<n; i++){
+            coin[i] = sc.nextInt();
+        }
+
+        int price = sc.nextInt();
+        System.out.println(solution(coin, price));
+    }
+
+    public static int solution(int[] coin, int price){
+        int[] cache = new int[price+1];
+        Arrays.fill(cache,0);
+
+        for(int i=0; i< cache.length; i++){
+            cache[i] = i/coin[0];
+        }
+
+        for(int i=1; i<coin.length; i++){
+            for(int j=coin[i]; j<cache.length; j++){
+                int each = cache[j-coin[i]] + 1;
+                cache[j] = Math.min(cache[j], each);
+            }
+        }
+        return cache[price];
+    }
 }
