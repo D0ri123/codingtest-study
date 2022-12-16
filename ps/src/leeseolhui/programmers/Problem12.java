7fa240893c79944e521862780d4c5b2b9840a548
d0ri123 <lsh81550@gmail.com>
2022-12-16 16:00:24 +0900

    resolve timeout 땅따먹기

--- ps/src/leeseolhui/programmers/Problem12.java
+++ ps/src/leeseolhui/programmers/Problem12.java
@@ -2,44 +2,29 @@ package leeseolhui.programmers;
 
 //땅따먹기
 
-public class Problem12 {
-    public static int[][] land;
-    public static int[][] check;
-    public static int sum = 0;
-    public static int max = Integer.MIN_VALUE;
+import java.util.Arrays;
 
+public class Problem12 {
     public static void main(String[] args){
-        Problem12 main = new Problem12();
-        land = new int[][]{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
-        System.out.println(main.solution(land));
+        int[][] land = new int[][]{{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+        Problem12 s = new Problem12();
+        System.out.println(s.solution(land));
     }
 
     int solution(int[][] land){
-        int answer;
-        check = new int[land.length+1][land[0].length];
-
-        for(int i=0; i<land[0].length; i++){
-            sum += land[0][i];
-            DFS(0, i);
-        }
-        answer = max;
-        return answer;
-    }
-
-    void DFS(int L, int idx){
-        if(L==land.length){
-            max = Math.max(max, sum);
-            sum = 0;
-        }else{
-            check[L][idx] = 1;
-            for(int i=0; i<check.length; i++){
-                if(check[L][i] == 0){
-                    check[L][i] = 1;
-                    sum += land[L][i];
-                    DFS(L+1, i);
-                    check[L][i] = 0;
+        for(int l=1; l<land.length; l++){
+            int[] arr = land[l];
+            for(int i=0; i<4; i++){
+                switch (i) {
+                    case 0 -> arr[0] += Math.max(Math.max(land[l - 1][1], land[l - 1][2]), land[l - 1][3]);
+                    case 1 -> arr[1] += Math.max(Math.max(land[l - 1][0], land[l - 1][2]), land[l - 1][3]);
+                    case 2 -> arr[2] += Math.max(Math.max(land[l - 1][0], land[l - 1][1]), land[l - 1][3]);
+                    case 3 -> arr[3] += Math.max(Math.max(land[l - 1][0], land[l - 1][1]), land[l - 1][2]);
                 }
             }
         }
+
+        int[] result = land[land.length-1];
+        return Arrays.stream(result).max().getAsInt();
     }
 }
