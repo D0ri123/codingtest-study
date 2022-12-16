7880ae937f226f7aa94783cf6104a4c720cfd845
d0ri123 <lsh81550@gmail.com>
2022-12-16 17:11:13 +0900

    solve 점프와 순간이동

--- ps/src/leeseolhui/programmers/Problem13.java
+++ ps/src/leeseolhui/programmers/Problem13.java
@@ -1,2 +1,27 @@
-package leeseolhui.programmers;public class Problem13 {
+package leeseolhui.programmers;
+
+//점프와 순간 이동
+
+import java.util.Scanner;
+
+public class Problem13 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n){
+        int[] cache = new int[n+1];
+        cache[1] = 1;
+        cache[2] = 1;
+        for(int i=3; i<=n;i++){
+            if(i%2 != 0){
+                cache[i] = cache[i-1] + 1;
+            } else {
+                cache[i] = cache[i/2];
+            }
+        }
+        return cache[n];
+    }
 }
