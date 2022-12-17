c8818bd2810ee527f7b11a427893b48d48029596
d0ri123 <lsh81550@gmail.com>
2022-12-17 18:13:32 +0900

    solve 동일한 부분합

--- ps/src/leeseolhui/dfs_bfs/Code17.java
+++ ps/src/leeseolhui/dfs_bfs/Code17.java
@@ -1,2 +1,36 @@
-package leeseolhui.dfs_bfs;public class Code17 {
+package leeseolhui.dfs_bfs;
+
+import java.util.Scanner;
+
+public class Code17 {
+    public static int totalSum = 0;
+    public static int n;
+    public static String answer = "NO";
+    public static boolean flag = false;
+    public static int[] arr;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+            totalSum += arr[i];
+        }
+        DFS(0, 0);
+        System.out.println(answer);
+    }
+
+    public static void DFS(int L, int sum){
+        if(flag) return;
+        if(L==n){
+            if(totalSum - sum == sum) {
+                flag = true;
+                answer = "YES";
+            }
+        }else{
+            DFS(L+1, sum + arr[L]);
+            DFS(L+1, sum);
+        }
+    }
 }
