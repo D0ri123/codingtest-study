4a4ddd12997b026b145ea25a24864faedc5e43cb
d0ri123 <lsh81550@gmail.com>
2023-01-01 14:02:27 +0900

    solve 교육과정 설계

---
+++ ps/src/leeseolhui/stack_queue/Code07.java
@@ -0,0 +1,36 @@
+package leeseolhui.stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Code07 {
+    public static Queue<Character> q;
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String curriculum = sc.nextLine(); //CBA
+        String plan = sc.nextLine(); //CBDAGE
+
+        System.out.println(solution(curriculum, plan));
+    }
+
+    public static String solution(String curriculum, String plan){
+        q = new LinkedList<>();
+        for(Character ch : curriculum.toCharArray()){
+            q.offer(ch); // A K D E F
+        }
+
+        for(Character ch : plan.toCharArray()){ // A Y K G D H E J
+            if(q.contains(ch)){
+                Character character = q.poll();
+                if(ch != character){
+                    return "NO";
+                }
+            }
+        }
+
+        if(!q.isEmpty()) return "NO";
+        return "YES";
+    }
+}
