ff7b542c1987ce5d1f02d724ea58e90568bd37a8
d0ri123 <lsh81550@gmail.com>
2023-01-08 16:31:06 +0900

    solve 네트워크

---
+++ ps/src/leeseolhui/programmers/Problem15.java
@@ -0,0 +1,60 @@
+package leeseolhui.programmers;
+
+//네트워크, union-find 사용
+
+import java.util.Scanner;
+
+public class Problem15 {
+    public static int[] parent;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] computers = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                computers[i][j] = sc.nextInt();
+            }
+        }
+        Problem15 pro = new Problem15();
+        System.out.println(pro.solution(n, computers));
+    }
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        parent = new int[n];
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+        }
+
+        boolean[][] visited = new boolean[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==j) continue;
+                if(computers[i][j] == 1 && computers[j][i] == 1) {
+                    if(!visited[i][j] && !visited[j][i]) {
+                        union(i, j);
+                        visited[i][j] = true;
+                        visited[j][i] = true;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(parent[i]==i) answer++;
+        }
+        return answer;
+    }
+
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a==b) return;
+        parent[a] = b;
+    }
+
+    public static int find(int a){
+        if(a == parent[a]) return a;
+        return parent[a] = find(parent[a]);
+    }
+}
