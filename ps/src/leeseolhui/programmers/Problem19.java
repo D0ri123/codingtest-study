29563a2f967d3dffbfff8d40a892e9e84a853b8b
d0ri123 <lsh81550@gmail.com>
2023-01-13 16:17:10 +0900

    solve 섬 연결하기

--- ps/src/leeseolhui/programmers/Problem19.java
+++ ps/src/leeseolhui/programmers/Problem19.java
@@ -1,2 +1,52 @@
-package leeseolhui.programmers;public class Problem19 {
+package leeseolhui.programmers;
+
+//섬 연결하기
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Problem19 {
+    public static int[] parent;
+
+    public static void main(String[] args){
+        Problem19 pro = new Problem19();
+        int n = 5;
+        int[][] costs = new int[][] {{0, 1, 5}, {1, 2, 3}, {2, 3, 3}, {3, 1, 2}, {3, 0, 4}, {2, 4, 6}, {4, 0, 7}};
+        parent = new int[n];
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+        }
+        System.out.println(pro.solution(n, costs));
+    }
+
+    public int solution(int n, int[][] costs){ //한 행에 [섬의 번호, 섬의 번호, 연결 비용]
+        int answer = 0;
+        Arrays.sort(costs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2]-o2[2];
+            }
+        });
+
+        for(int i=0; i<costs.length; i++){
+            if(!union(costs[i][0], costs[i][1]))
+                answer += costs[i][2];
+        }
+        return answer;
+    }
+
+    public static boolean union(int u, int v){
+        u = find(u);
+        v = find(v);
+        if(u==v) return true;
+        else {
+            parent[u] = v;
+            return false;
+        }
+    }
+
+    public static int find(int u){
+        if(u == parent[u]) return u;
+        return parent[u] = find(parent[u]);
+    }
 }
